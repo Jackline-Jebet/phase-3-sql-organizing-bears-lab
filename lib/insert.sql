e154e108857c8c64498d0a821522e1818491fc85
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-21 12:49:19 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (
+id,
+name,
+age,
+sex,
+color,
+temperament,
+alive
+
+) VALUES
+(1,'Tabitha', 6, 'F', 'DARK', 'COLD', 1),
+(2,'Grinch', 2, 'M', 'BROWN', 'COLD', 0),
+(3,'Wendy', 6, "F", 'BLACK', 'COLD', 1 ),
+(4,'Rowdy', 10, 'M', 'WHITE', 'WARM', 1),
+(5, 'Melissa', 13, 'F', 'BLUE', 'NICE', 1),
+(6,'Sargeant Brown', 19, 'M', 'Green', 'fatty', 0 ),
+(7,'Mr. Chocolate', 23, 'M', 'BLACK', 'COLD', 0),
+(8,null, 26, 'M', 'BLACK', 'WARM', 0);
